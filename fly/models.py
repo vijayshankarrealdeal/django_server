d9620999746785393836b45f441c5a35b185c53c
vijayshankarrealdeal <vijaygaya056@gmail.com>
2021-12-30 23:26:01 +0530

    nnddd

--- fly/models.py
+++ fly/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 class FlightBoard(models.Model):
     departure = models.CharField(max_length=100)
-    time = models.DateTimeField()
+    time = models.CharField(max_length=100)
     flight = models.CharField(max_length=100)
     airline = models.CharField(max_length=100)
     info_url = models.CharField(max_length=100)
     status = models.CharField(max_length=100)
     is_departure = models.BooleanField()
+    
 
     def __str__(self) -> str:
         return self.departure
