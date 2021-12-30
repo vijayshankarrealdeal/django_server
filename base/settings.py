828133151674575018d674f08c82bcfe4d77a452
vijayshankarrealdeal <vijaygaya056@gmail.com>
2021-12-30 22:01:06 +0530

    jd

--- base/settings.py
+++ base/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-u0hqdj7hqud*s@&vkxc@5_*%5$w#n(75-o(wvual(4a9n+i@7)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
+CSRF_TRUSTED_ORIGINS = ['serverxx.azurewebsites.net']
 ALLOWED_HOSTS = [
     'serverxx.azurewebsites.net'
 ]
