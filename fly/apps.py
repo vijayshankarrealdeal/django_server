74706e722eaf83be448194d002ffa114739500cd
vijayshankarrealdeal <vijaygaya056@gmail.com>
2021-12-31 10:59:53 +0530

    all_reg_done

--- fly/apps.py
+++ fly/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class FlyConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'fly'
+
+    def ready(self):
+        import fly.signals
