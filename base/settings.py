8a976f50031556cfd8b9083d5a7e273e7fa42b24
vijayshankarrealdeal <vijaygaya056@gmail.com>
2022-01-08 15:35:25 +0530

    asdds

--- base/settings.py
+++ base/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     # 'serverxx.azurewebsites.net',
-    'https://serverxxx.azurewebsites.net',
+    'serverxxx.azurewebsites.net',
     '127.0.0.1',
     '10.0.2.2'
 ]
